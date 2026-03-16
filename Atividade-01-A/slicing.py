d04987f0dd25586fcaa37df6329054ec2cb3f523
GabrielDavi7 <gabrielbaiamota765@gmail.com>
2026-03-16 19:21:27 -0300

    Add degrade

--- Atividade-01-A/slicing.py
+++ Atividade-01-A/slicing.py
@@ -15,3 +15,18 @@ img[50:150, 50:150] = [255,255,255]
 plt.imshow(img) 
 plt.axis("off") #deixa so imagem
 plt.show() #imprime tela
+
+
+    #degrade
+
+#imagem 200 × 200, desta vez contendo um degrade horizontal, indo do lado preto (0) na esquerda ate branco (255) na direita.
+
+y,x = 200,200
+degrade = np.linspace(0,255,x,dtype=np.uint8) #0,255 para sair de preto e ir a branco passa x pq é horizontal
+
+img_gray = np.tile(degrade, (y, 1))
+img_2 = np.stack([img_gray]*3, axis=2)
+
+plt.imshow(img_2) 
+plt.axis("off") #deixa so imagem
+plt.show() 
