5db70c3e538786835ab1d4eba2bb5b815dcca8a7
GabrielDavi7 <gabrielbaiamota765@gmail.com>
2026-03-16 21:32:01 -0300

    Add histograma

--- Atividade-01-A/statistics_threshold.py
+++ Atividade-01-A/statistics_threshold.py
@@ -4,23 +4,31 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = "img/q2_a_original.png"
-img= plt.imread(path)
-
-#media dos canais
-media_canais = np.mean(img, axis=2)#como pedido
-print("Média dos Canais:", media_canais)
-
-#conversão
-img_gray_uint8 = (media_canais * 255).astype(np.uint8)#converte para uint8
-plt.imshow(img_gray_uint8, cmap='gray') 
-plt.title("colormap cinza.")
-plt.axis("off") #deixa so imagem
-plt.show()
-
-
-
 output_folder = "save_replies_image"
 os.makedirs(output_folder, exist_ok=True)  
+path = "img/q2_a_original.png"
+img = plt.imread(path)
 
-plt.imsave(os.path.join(output_folder, "question_3_MediaGray.png"), img_gray_uint8)
+    # Media dos canais e converte para cinza
+media_canais = np.mean(img, axis=2)
+img_gray_uint8 = (media_canais * 255).astype(np.uint8)
+
+plt.imshow(img_gray_uint8, cmap='gray') 
+plt.title("Imagem em tons de cinza")
+plt.axis("off")
+plt.show()
+
+    #Media e plotar histograma
+media_total = np.mean(img_gray_uint8)
+print("Média total:", media_total)
+
+plt.figure(figsize=(8,5))
+plt.hist(img_gray_uint8.flatten(), bins=255, range=(0,255), color='gray')
+plt.title(f"Histograma da imagem em tons de cinza: {media_total:.2f}")
+plt.xlabel("Intensidade")
+plt.ylabel("Número de pixels")
+plt.savefig(os.path.join(output_folder, "HistogramaMedia.png"))
+plt.show()
+plt.close()  # fecha a figura
+
+plt.imsave(os.path.join(output_folder, "question_3_MediaGray.png"), img_gray_uint8, cmap='gray')
