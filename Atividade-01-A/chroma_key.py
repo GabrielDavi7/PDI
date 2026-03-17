d1b01a209985594a4a7921780046bc1f9f3d0fbd
GabrielDavi7 <gabrielbaiamota765@gmail.com>
2026-03-16 22:13:02 -0300

    add chroma key

--- Atividade-01-A/chroma_key.py
+++ Atividade-01-A/chroma_key.py
@@ -28,10 +28,8 @@ y, x, canais = img.shape
 
 new_fundo = np.zeros(img.shape, dtype=img.dtype) # Cria a tela preta
 
-# Linha do degradê: começa no Azul Escuro (0.6) e vai até o Preto (0.0) na largura (x)
-coluna_degrade = np.linspace(0.0, 0.6, y).reshape(-1, 1)
+coluna_degrade = np.linspace(0.0, 0.6, y).reshape(-1, 1) #começa no Azul Escuro (0.6) e vai até o Preto (0.0) na altura (y)
 matriz_degrade = np.tile(coluna_degrade, (1,x))
-
 new_fundo[:, :, 2] = matriz_degrade #aplica degrade somente no rgb azul  
 
 plt.imshow(new_fundo)
@@ -40,9 +38,22 @@ plt.axis("off")
 plt.show()
 
 
+    #Aplica Chroma key
+
+img_chroma = img.copy() #copia imagem
+img_chroma [mask_ceu] = new_fundo[mask_ceu] #pega os pixels do ceu separado na questão A e os pixels do ceu da imagem gerada na questão B
+
+plt.imshow(img_chroma)
+plt.title("Imagem Com Chroma Key")
+plt.axis("off")
+plt.show()
+
+
+#salva imagens
 output_folder = "save_replies_image"
 os.makedirs(output_folder, exist_ok=True)  
 print("Imagens salvas na pasta:", output_folder)
 plt.imsave(os.path.join(output_folder, "question_4_FatiamentoCeu.png"), mask_ceu)
 plt.imsave(os.path.join(output_folder, "question_4_NovoFundo.png"), new_fundo)
+plt.imsave(os.path.join(output_folder, "question_4_ChromaKey.png"), img_chroma)
 
